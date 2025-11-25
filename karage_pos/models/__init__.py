d634f86943b5f62f256cb1445f0a25b2deaca7b6
Mohammad Abu Musa <m.abumusa@gmail.com>
2025-11-25 08:08:25 +0000

    Fix isort formatting to match CI expectations

--- karage_pos/models/__init__.py
+++ karage_pos/models/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
-from . import karage_pos_config, webhook_idempotency, webhook_log, webhook_sender
+from . import (karage_pos_config, webhook_idempotency, webhook_log,
+               webhook_sender)
